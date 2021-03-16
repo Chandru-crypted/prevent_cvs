6e3a0640cd0bdbdbf38ed241b5a494f00030cb75
Chandru-crypted <chander.sekaran12@gmail.com>
2021-03-16 14:02:37 +0530

    make a file to take any FPS and change it into 15FPS

---
+++ CODE/Installation and test_installation/testing_fast_15FPS.py
@@ -0,0 +1,210 @@
+# we can go two approach try and 
+# 1) do fast facial landmark with static video and EAR thing  
+# 2) or we can go live approach and try EAR ratio in that 
+
+# -------------------- 1st approach ----------------------------
+
+
+# import the necessary packages
+from scipy.spatial import distance as dist
+from imutils.video import FileVideoStream
+from imutils.video import VideoStream
+from imutils import face_utils
+import numpy as np
+import argparse
+import imutils
+import time
+import dlib
+import cv2
+
+
+def eye_aspect_ratio(eye):
+	# eye contains the coordinate of both left and right eye points
+	# compute the euclidean distances between the two sets of
+	# vertical eye landmarks (x, y)-coordinates
+	A = dist.euclidean(eye[1], eye[5])
+	B = dist.euclidean(eye[2], eye[4])
+
+	# compute the euclidean distance between the horizontal
+	# eye landmark (x, y)-coordinates
+	C = dist.euclidean(eye[0], eye[3])
+
+	# compute the eye aspect ratio
+	ear = (A + B) / (2.0 * C)
+
+	# return the eye aspect ratio
+	return (ear)
+
+
+# Usage
+# python testing_fast_facial_landmark.py --shape-predictor shape_predictor_68_face_landmarks.dat --video blink_detection_demo.mp4
+# for argument that we can pass in command prompt
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape-predictor", required=True,
+	help="path to facial landmark predictor")
+ap.add_argument("-v", "--video", type=str, default="",
+	help="path to input video file")
+args = vars(ap.parse_args())
+
+
+# define two constants, one for the eye aspect ratio to indicate
+# blink and then a second constant for the number of consecutive
+# frames the eye must be below the threshold
+EYE_AR_THRESH = 0.3
+EYE_AR_CONSEC_FRAMES = 2 # ------- change 
+
+# Since we are skipping frames in fast processing should we change this const that is above
+
+# initialize the frame counters and the total number of blinks
+COUNTER = 0
+TOTAL = 0
+
+# initialize dlib's face detector (HOG-based) and then create
+# the facial landmark predictor
+print("[INFO] loading facial landmark predictor...")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(args["shape_predictor"])
+
+# grab the indexes of the facial landmarks for the left and
+# right eye, respectively
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+# start the video stream thread
+print("[INFO] starting video stream thread...")
+vs = FileVideoStream(args["video"]).start()
+fileStream = True
+# vs = VideoStream(src=0).start()
+# vs = VideoStream(usePiCamera=True).start()
+# fileStream = False
+time.sleep(1.0)
+
+
+# to process the fast detection, as we told before in the above theory we have to fixed the size of the frame and run the
+# face detection and landmarks on that frame and later we scale the output co-ordinates value with the original frame. So
+# here we kept the size of the frame is 480.
+heightResize = 480
+
+# here we are specifying how many frames it has to skipped in the video file stream so that it will deduct fast face detection. 
+a_batch = 2
+noofframes_to_process = 1
+noofframes_to_skip = 1
+temp_batch_counter = 1
+temp_to_skip_counter = 1
+temp_to_process_counter = 1
+# i will process the frames 
+# then i will skip it
+
+# loop over frames from the video stream
+# frame count that we need
+frame_counter = 0
+frame_processed_counter = 0
+
+while True:
+
+	# if this is a file video stream, then we need to check if
+	# there any more frames left in the buffer to process
+	if fileStream and not vs.more():
+		break
+
+	# grab the frame from the threaded video file stream, resize
+	# it, and convert it to grayscale
+	# channels)
+	frame = vs.read()
+
+	#frame = imutils.resize(frame, width=450)
+	# this actually keeps the aspect ratio intact 
+
+	# https://answers.opencv.org/question/208857/what-is-the-difference-between-cv2resize-and-imutilsresize/ 
+	# and also in the interpolation the fast facial landmark has given cv2.INTER_LINEAR 
+	# but as you check the website we can see that the defualt in imutils is cv2.INTER_AREA
+
+	# we are throwing even frames , as we are going to process only the odd frames
+	if (temp_to_process_counter <= noofframes_to_process):
+		frame_processed_counter += 1
+		if frame is not None:
+			frame = imutils.resize(frame, width=450)
+		else:
+			break
+		#frame = imutils.resize(frame, width = 450, height = 450) # ____________________ (1)
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		# detect faces in the grayscale frame
+		rects = detector(gray, 0)
+
+		# loop over the face detections
+		for rect in rects:
+			# determine the facial landmarks for the face region, then
+			# convert the facial landmark (x, y)-coordinates to a NumPy
+			# array
+			shape = predictor(gray, rect)
+			shape = face_utils.shape_to_np(shape)
+
+			# extract the left and right eye coordinates, then use the
+			# coordinates to compute the eye aspect ratio for both eyes
+			leftEye = shape[lStart:lEnd]
+			rightEye = shape[rStart:rEnd]
+			leftEAR = eye_aspect_ratio(leftEye)
+			rightEAR = eye_aspect_ratio(rightEye)
+
+			# average the eye aspect ratio together for both eyes
+			ear = (leftEAR + rightEAR) / 2.0
+
+			# can i delete this it is just visulazing eye points
+			# # compute the convex hull for the left and right eye, then
+			# # visualize each of the eyes
+			# leftEyeHull = cv2.convexHull(leftEye)
+			# rightEyeHull = cv2.convexHull(rightEye)
+			# cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+			# cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+			# check to see if the eye aspect ratio is below the blink
+			# threshold, and if so, increment the blink frame counter
+			if ear < EYE_AR_THRESH:
+				COUNTER += 1
+
+			# otherwise, the eye aspect ratio is not below the blink
+			# threshold
+			else:
+				# if the eyes were closed for a sufficient number of
+				# then increment the total number of blinks
+				if COUNTER >= EYE_AR_CONSEC_FRAMES:
+					TOTAL += 1
+				# reset the eye frame counter
+				COUNTER = 0
+			
+			# draw the total number of blinks on the frame along with
+			# the computed eye aspect ratio for the frame
+			cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+			cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+		
+		temp_to_process_counter += 1
+		temp_batch_counter += 1
+		# show the frame
+		cv2.imshow("Frame", frame)
+		key = cv2.waitKey(1) & 0xFF
+		# if the `q` key was pressed, break from the loop
+		if key == ord("q"):
+			break
+	elif (temp_to_skip_counter <= noofframes_to_skip):
+		temp_to_skip_counter += 1
+		temp_batch_counter += 1
+	else: 
+		temp_batch_counter = 1
+		temp_to_process_counter = 1
+		temp_to_skip_counter = 1
+	
+	# increment frame counter 
+	frame_counter += 1 
+
+
+print("\n")
+print("\nThe total frames in video {}".format(frame_counter))
+print("\nThe FPS of the original video is total (frames / duration) of the video")
+print("\nThe number of frames processed is {}".format(frame_processed_counter))
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
